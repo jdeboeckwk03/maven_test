95740e5ea9bd8cb691911b1493e0f27e3e8655ed
Jelle De Boeck <jelle.deboeck@wolterskluwer.com>
2021-11-16 16:26:44 +0100

    First Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Jef";
+        System.out.printf( "Hello World!, my name is %s", name );
+    }
+}
